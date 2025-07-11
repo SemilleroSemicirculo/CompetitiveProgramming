05c16bceaf6f4ace1eb8efcc108ed18aa23f43cb
Sebastián Caballero <juan.caballero03@usa.edu.co>
2025-07-11 00:31:29 -0500

    Plantilla de C++

---
+++ Plantillas/Plantilla.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll INF = numeric_limits<ll>::max();
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define MOD 998244353
+// #define MOD pow(10, 9)+7;
+// #define MOD 3245284303;
+#define pi acos(-1)
+#define gcd __gcd
+#define lcm(a, b) ((a) * (b) / gcd(a, b))
+#define precision(n) cout << fixed << setprecision(n)
+#define debug(arr) cerr << #arr << " = "; for (auto &x : arr) cerr << x << " "; cerr << endl;
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+ 
+
+int main(){
+    
+}
